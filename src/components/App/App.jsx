29bce3ddcab6b08da21050e8dc91b00f160c2369
Will G Piro <willgpiro@gmail.com>
2020-05-07 08:32:09 -0700

    updated Provider import

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import CharacterContainer from '../../container/CharacterContainer.jsx';
 import Header from '../Header/Header.jsx';
-import { BeliveOrDenyProvider } from '../hooks/BelieveOrDenyProvider';
+import { BelieveOrDenyProvider } from '../hooks/BelieveOrDenyProvider';
 
 export default function App() {
   return ( 
-    <BeliveOrDenyProvider>
+    <BelieveOrDenyProvider>
       <Header />
       <CharacterContainer />
-    </BeliveOrDenyProvider>
+    </BelieveOrDenyProvider>
   );
 }
 
