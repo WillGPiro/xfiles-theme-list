3e99d1d59c1c31ad87c04b9053b79aa1f1b11bf7
Will G Piro <willgpiro@gmail.com>
2020-05-07 14:05:01 -0700

    pagination works!

--- src/container/CharacterContainer.jsx
+++ src/container/CharacterContainer.jsx
@@ -7,21 +7,27 @@ import { useBelieveDeny } from '../components/hooks/BelieveOrDenyProvider.jsx';
 
 const CharacterContainer = () => {
   const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
  
   const theme = useBelieveDeny();
   const stylesToUse = theme === 'believe' ? stylesBelieve : stylesDeny; 
 
   useEffect(() => {
-    fetchTheTruthIsOutThere()
+    fetchTheTruthIsOutThere(page)
       .then(allCharacters => setCharacters(allCharacters));
-  }, []);
+  }, [page]);
 
   const xfileCharacters = characters.map(character => {
     return <CharacterList key={character.name} {...character} />;
   });
- 
+
+  const increment = () => setPage(prevPage => prevPage + 1);
+  const decrement = () => setPage(prevPage => prevPage - 1);
+
   return (
     <ul className={stylesToUse.Truth}>
+      <button onClick={decrement}>&lt;</button>
+      <button onClick={increment}>&gt;</button>
       {xfileCharacters} 
     </ul>
   );
--- src/services/xfileService.js
+++ src/services/xfileService.js
@@ -1,5 +1,5 @@
-export const fetchTheTruthIsOutThere = () => {
-  return fetch('https://xfiles-api.herokuapp.com/api/v1/characters')
+export const fetchTheTruthIsOutThere = (page = 1) => {
+  return fetch(`https://xfiles-api.herokuapp.com/api/v1/characters?perPage=20&page=${page}`)
     .then(res => res.json())
     .then(json => json.results);
 };
